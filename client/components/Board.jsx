147cb59a32a7384bd4c352299337bfb180a69c09
jeff-seymour <73873192+jeff-seymour@users.noreply.github.com>
2021-02-23 16:19:44 +1300

    okok

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -3,32 +3,34 @@ import Tile from './Tile'
 import boardTiles from '../boardTiles'
 
 function Board (props) {
-  return <div className='tiles'>
-    {boardTiles.map(row => { 
-      return  <div className="row">
-        { 
-          row.map (tile => {
-            console.log(tile)
-            return <Tile
-              id={tile.id}
-              board={tile.Board}
-              tower={tile.tower}
-              key={tile.id}
+  return <div className='playspace'>
+    {/* {tile.map(column => { */}
+      <div className="board">
+        {boardTiles.map(row => { 
+          return  <div className="row">
+            { 
+              row.map (tile => {
+                console.log(tile)
+                return <Tile
+                  id={tile.id}
+                  board={tile.Board}
+                  tower={tile.tower}
+                  key={tile.id}
+                  // info={tile.info}
+                  // value={tile.value}
+                  // isVisible={tile.isVisible} 
+                  />}) 
+              }
+            </div>})}
+      </div>
 
-              
+      <div className='notBoard'>
+        <h2>stuff goes here</h2>
+      </div>
 
-              // info={tile.info}
-              // value={tile.value}
-              // isVisible={tile.isVisible} 
-              />
-          }) 
-        } </div>
-    })}
-  </div>
+    </div>
 }
 
-function boardStatus (){
-}
 
+function boardStatus (){}
 export default Board
-
